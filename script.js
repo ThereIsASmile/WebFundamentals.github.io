6d5ac02df75336956fec488b40534d7c45a337f8
ThereIsASmile <57597467+ThereIsASmile@users.noreply.github.com>
2023-03-07 16:00:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("page loading...")
+
+// elements dissapear 
+function accept() {
+    rowThree.remove();
+}
+
+// click on city and alert will show loading weather report
+function loadingWeb() {
+    alert("loading Web Fundamentals...")
+}
+
+//change from HTML CSS JS
+function chooseHcj(value) {
+    alert("You chose " + value);
+}
